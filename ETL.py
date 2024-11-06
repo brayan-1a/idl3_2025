7c9c8e7f2ff4db2019f944dfe7b390f9b4ef7c43
brayan-1a <i2414619@continental.edu.pe>
2024-11-05 22:03:56 -0500

    Agregar cambios3

--- ETL.py
+++ ETL.py
@@ -5,7 +5,7 @@ from supabase import create_client, Client
 
 url = "https://adfoiwetcaqxzdudmbsz.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkZm9pd2V0Y2FxeHpkdWRtYnN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NTk5NzcsImV4cCI6MjA0NjQzNTk3N30.W7b6z-leiI_igua5iND3FcoHaF5iGTjRf_DSBP0UDRg"
-
+supabase: Client = create_client(url, key)
 
 csv_path = r"C:\Users\BRAYAN\Downloads\ventas_cafe.csv"
 
